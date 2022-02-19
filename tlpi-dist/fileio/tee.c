ef3ff29d85c1c56d2a494c9b902d8e280d1d17b5
root <gkgklabs6@gmail.com>
2022-02-19 15:00:25 +0530

    first draft of tee.c command

--- tlpi-dist/fileio/tee.c
+++ tlpi-dist/fileio/tee.c
@@ -12,15 +12,18 @@
 #include <ctype.h>
 #include "tlpi_hdr.h"
 
+#ifndef BUF_SIZE
+#define BUF_SIZE 1024
+#endif 
+
 int
 main(int argc, char *argv[])
 {
-    size_t len;
-    off_t offset;
-    int fdwr,fdr = STDIN_FILENO, op, j;
-    unsigned char *buf;
+    ssize_t numRead;
+    int fdwr,fdr = STDIN_FILENO;
+    unsigned char *buf[BUF_SIZE];
 
-    if (argc < 2 || strcmp(argv[1], "--help") == 0)
+    if (argc >= 2 && strcmp(argv[1], "--help") == 0)
         usageErr("%s file ...\n",argv[0]);
 
     if (argc >  1){
@@ -34,9 +37,13 @@ main(int argc, char *argv[])
     if (fdwr == -1)
         errExit("open");
 
-
+    while( (numRead = read(fdr, buf, BUF_SIZE)) > 0) {
+    	if (write(fdwr, buf, numRead)!=numRead) {
+		fatal("write() returned error or partial write occured");		
+	}	
+    }
     if (close(fdwr) == -1)
         errExit("close");
-
+  
     exit(EXIT_SUCCESS);
 }
